976f6d6709abbd1fbe2e8d934e13b8d89356fb67
Felipe-Lopes-code <cash.oliveira14@gmail.com>
2025-08-08 21:09:29 -0300

    Conf. finais antes da entrega do projeto

--- app/tests/mocks/routes/user-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login } = require('../../src/middlewares/authMiddleware');
-
-router.post('/login', login);
-
-module.exports = router;
